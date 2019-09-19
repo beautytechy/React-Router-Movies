715ea904a498c951bca695be498910ceda92f3dc
Nicole Hollis <nicole@emmabeauty.co>
2019-09-19 11:56:12 -0700

    MVP Complete

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {Route} from "react-router-dom"
 import SavedList from './Movies/SavedList';
 import Movie from "./Movies/Movie"
+import MovieList from "./Movies/MovieList"
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -13,8 +14,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <Route exact path="/" component={Movie} />
-      <Route path="/movies/:id" component={Movie} />
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" render={props=>(<Movie {...props} />)} />
     </div>
   );
 };
